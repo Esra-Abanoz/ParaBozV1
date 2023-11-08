364ae97125e8a2ac264a32de1274b98e2f70ac15
Batuhan Bayrak <71524853+Paramana16@users.noreply.github.com>
2023-11-08 19:28:06 +0300

    Created Project Folders

---
+++ ParaBoz.UI/Controller/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Nodes;
+
+namespace ParaBoz.UI.Controller
+{
+    public class HomeController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return new JsonObject;
+        }
+    }
+}
